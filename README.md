1fbf17e6e5c8df12ce4996bc160063a792c42f9f
rudenoise <rudenoise@gmail.com>
2020-07-03 16:25:27 +0100

    update conf and basics

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # av_groovebox_prototype_01
+
 A prototype tool for creating audio/visual output, by reacting to audio and midi inputs
 
-Basic env
+Basic env and dependencies
+
 ```bash
 node --version
 v12.16.0
+
 npm --version
 6.14.5
+
+entr
+release: 4.6
+
+tidy --version
+HTML Tidy for Mac OS X released on 31 October 2006 - Apple Inc. build 16.4
+# with a ~/.tidyrc
+# http://api.html-tidy.org/tidy/tidylib_api_5.2.0/tidy_config.html
 
 # install
 npm i
--- main.js
+++ main.js
@@ -1,7 +1,51 @@
-const x = 2
+const { app, BrowserWindow, ipcMain } = require('electron')
 
-console.log(x)
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
-const a = ''
+  console.log('app started')
 
-const b = 4
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  console.log('index loaded')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+  console.log('devtools open')
+}
+
+ipcMain.on('pingMsg', function (event, arg) {
+  console.log(arg + ' recieved in main')
+  event.reply('pongMsg', 'pong')
+  console.log('pongMsg sent')
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
